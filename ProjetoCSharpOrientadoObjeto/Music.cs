0b478a7bac5dc13062b29beaa6639ec3f91dcef0
Lucas Teixeira <GibirinoGamer@hotmail.com>
2023-05-30 22:33:58 -0300

    Refactoring and removing lots of Console.WriteLine

--- ProjetoCSharpOrientadoObjeto/Music.cs
+++ ProjetoCSharpOrientadoObjeto/Music.cs
@@ -4,4 +4,20 @@
     public string artist;
     public int duration;
     public bool available;
+
+    public void ShowTechnicalForm()
+    {
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Artist: {artist}");
+        Console.WriteLine($"Duration: {duration}");        
+        if (available)
+        {
+            Console.WriteLine("Available in the plan");
+        } 
+        else
+        {
+            Console.WriteLine("Pick other plan more expansive");
+        }
+
+    }
 }
--- ProjetoCSharpOrientadoObjeto/Program.cs
+++ ProjetoCSharpOrientadoObjeto/Program.cs
@@ -1,16 +1,24 @@
 ﻿// Beginning of the studies about object in c#
 //App de musica que precisa ter name / artist / duration / available
 
-Music musica1 = new Music();
+Music music1 = new Music();
+music1.name = "Máscaras";
+music1.artist = "Shiny_sz";
+music1.duration = 280;
+music1.available = true;
 
-musica1.name = "Máscaras";
-musica1.artist = "Shiny_sz";
+music1.ShowTechnicalForm();
+
 //Console.WriteLine($"Nome da música {musica1}")It is show for default the type of object when you dont define.
-Console.WriteLine($"The name of the artist is: {musica1.artist}");
-Console.WriteLine($"The name of the music is: {musica1.name}");
-Music musica2 = new Music();
-musica2.name = "Chainsaw Man";
-musica2.artist = "VMZ";
+
+Music music2 = new Music();
+music2.name = "Chainsaw Man";
+music2.artist = "VMZ";
+music2.duration = 300;
+music2.available = false;
+
+music2.ShowTechnicalForm();
+
 
 
 
