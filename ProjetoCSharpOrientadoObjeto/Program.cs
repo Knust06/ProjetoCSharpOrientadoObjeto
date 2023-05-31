359ae40304e7bd70512a27086bbcdfe58b800a85
Lucas Teixeira <GibirinoGamer@hotmail.com>
2023-05-30 22:22:11 -0300

    Beginning of the studies and creating Music.cs

--- ProjetoCSharpOrientadoObjeto/Program.cs
+++ ProjetoCSharpOrientadoObjeto/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Salve Família");
+﻿// Beginning of the studies about object in c#
+//App de musica que precisa ter name / artist / duration / available
+
+Music musica1 = new Music();
+
+musica1.name = "Máscaras";
+musica1.artist = "Shiny_sz";
+//Console.WriteLine($"Nome da música {musica1}")It is show for default the type of object when you dont define.
+Console.WriteLine($"The name of the artist is: {musica1.artist}");
+Console.WriteLine($"The name of the music is: {musica1.name}");
+Music musica2 = new Music();
+musica2.name = "Chainsaw Man";
+musica2.artist = "VMZ";
+
+
+
