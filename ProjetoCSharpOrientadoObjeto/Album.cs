11609f554a9fe5607ffae560c511e5235ee6512b
Lucas Teixeira <GibirinoGamer@hotmail.com>
2023-07-22 14:51:41 -0300

    Test of going back

--- ProjetoCSharpOrientadoObjeto/Album.cs
+++ ProjetoCSharpOrientadoObjeto/Album.cs
@@ -2,16 +2,16 @@
 {
     public string Name { get; set; }
 
-    private List<Music> Musics = new List<Music>();
-    public int TotalDuration => Musics.Sum(Music => Music.Duration);
+    private List<Music> musics = new List<Music>();
+    public int TotalDuration => musics.Sum(Music => Music.Duration);
     public void AddMusic(Music music)
     {
-        Musics.Add(music);
+        musics.Add(music);
     }
     public void ShowAlbumMusics()
     {
         Console.WriteLine($"List of musics in album {Name}\n");
-        foreach (Music music in Musics)
+        foreach (Music music in musics)
         {            
             Console.WriteLine($"Music: {music.Name}");            
         }
