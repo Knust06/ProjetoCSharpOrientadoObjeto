797037fca1452fcf94015e35a1161fd79a2d194a
Lucas Teixeira <GibirinoGamer@hotmail.com>
2023-07-29 09:37:51 -0300

    Ending the project and getting the certificate

---
+++ ProjetoCSharpOrientadoObjeto/Podcast.cs
@@ -0,0 +1,27 @@
+﻿class Podcast
+{
+    private List<Episode> episodes = new();
+    public Podcast(string name, string host)
+    {
+        Name = name;
+        Host = host;
+    }
+
+    public string Name { get; }
+    public string Host { get; }
+    public int TotalEpisodes => episodes.Count;
+
+    public void AddEpisode(Episode episode)
+    {
+        episodes.Add(episode);
+    }
+
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Podcast {Name} presented by {Host}\n");
+        foreach (Episode episode in episodes.OrderBy(e => e.Order)) {
+            Console.WriteLine(episode.Summary);
+        }
+        Console.WriteLine($"\n\nThis Podcast have {TotalEpisodes} episodes");
+    }
+}
--- ProjetoCSharpOrientadoObjeto/Program.cs
+++ ProjetoCSharpOrientadoObjeto/Program.cs
@@ -1,26 +1,37 @@
-﻿Banda Shaw_Mendes = new Banda("Shaw_Mendes");
+﻿//Banda Shaw_Mendes = new Banda("Shaw_Mendes");
+//
+//Album albumOfShawn = new Album("Illuminate");
+//
+//Music music1 = new Music(Shaw_Mendes, "Mercy")
+//{
+//    Duration = 190,
+//    Available = false
+//};
+//Music music2 = new Music(Shaw_Mendes, "There is nothing holding me back")
+//{
+//    Duration = 300,
+//    Available = true
+//};
+//
+//albumOfShawn.AddMusic(music1);
+//albumOfShawn.AddMusic(music2);
+//albumOfShawn.ShowAlbumMusics();
+//Shaw_Mendes.AddAlbum(albumOfShawn);
+//Shaw_Mendes.ShowDiscograph();
+//
+//music1.ShowTechnicalForm();
+//music2.ShowTechnicalForm();
 
-Album albumOfShawn = new Album("Illuminate");
+Episode ep1 = new(2, "Facility Tecnique", 45);
+ep1.AddGuest("Knust");
+ep1.AddGuest("Isabella");
 
-Music music1 = new Music(Shaw_Mendes, "Mercy")
-{
-    Duration = 190,
-    Available = false
-};
-Music music2 = new Music(Shaw_Mendes, "There is nothing holding me back")
-{
-    Duration = 300,
-    Available = true
-};
+Episode ep2 = new(1, "New creating content", 32);
+ep2.AddGuest("Thiago");
+ep2.AddGuest("Lobo");
 
-albumOfShawn.AddMusic(music1);
-albumOfShawn.AddMusic(music2);
-albumOfShawn.ShowAlbumMusics();
-Shaw_Mendes.AddAlbum(albumOfShawn);
-Shaw_Mendes.ShowDiscograph();
-
-music1.ShowTechnicalForm();
-music2.ShowTechnicalForm();
-
-
+Podcast podcast = new("New podcast", "Daniel");
+podcast.AddEpisode(ep1);
+podcast.AddEpisode(ep2);
+podcast.ShowDetails();
 
